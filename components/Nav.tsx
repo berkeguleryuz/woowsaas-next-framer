44ed2b7699f2fd0e4782d5697a30cd1824157afc
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-09-08 15:02:41 +0200

    update

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -12,7 +12,14 @@ const Nav = (props: Props) => {
             <Image src="/logo.svg" alt="logo" width={100} height={100} />
           </div>
           <div>
-            <div className="size-10 relative"></div>
+            <div className="size-10 relative">
+              <div className="absolute left-1/2 top-1/2 -translate-x-1/2  -translate-y-1/2">
+                <div className="w-5 h-0.5 bg-zinc-300 -translate-y-1"></div>
+              </div>
+              <div className="absolute left-1/2 top-1/2 -translate-x-1/2  -translate-y-1/2">
+                <div className="w-5 h-0.5 bg-zinc-300 translate-y-1"></div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
