946659fb8555b1e30594fb0fa22c7480bbc7d548
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-09-12 14:13:10 +0200

    update

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,13 +5,14 @@ type Props = {};
 
 const Nav = (props: Props) => {
   return (
-    <header>
+    <header className="sticky container mx-auto top-0 bg-neutral-900/50">
       <div className="container">
-        <div className="flex justify-between">
+        <div className="flex justify-between items-center h-24">
           <div>
-            <Image src="/logo.svg" alt="logo" width={100} height={100} />
+            <Image src="/logo.png" alt="logo" width={77} height={77} />
           </div>
-          <div>
+          <div className="flex gap-4 items-center">
+            <button className="bg-fuchsia-500/20 px-4 py-2 font-extrabold uppercase">Get Started</button>
             <div className="size-10 relative">
               <div className="absolute left-1/2 top-1/2 -translate-x-1/2  -translate-y-1/2">
                 <div className="w-5 h-0.5 bg-zinc-300 -translate-y-1"></div>
