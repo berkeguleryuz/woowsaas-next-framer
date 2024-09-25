93af57406f1c68e91a491cd90480063bdbf58662
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-09-25 20:47:58 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import FeaturesCards from "@/components/FeaturesCards";
 import FeaturesGrid from "@/components/FeaturesGrid";
 import Hero from "@/components/Hero";
+import LatestPosts from "@/components/LatestPosts";
 import Nav from "@/components/Nav";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <FeaturesCards />
       <FeaturesGrid />
+      <LatestPosts />
     </main>
   );
 }
---
+++ components/LatestPosts.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const LatestPosts = (props: Props) => {
+  return <div>LatestPosts</div>;
+};
+
+export default LatestPosts;
