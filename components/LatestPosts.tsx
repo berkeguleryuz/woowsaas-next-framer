08ce4335a4699fe3718a8bc552b4157d9d8000a6
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-10-01 19:15:22 +0200

    update

--- components/LatestPosts.tsx
+++ components/LatestPosts.tsx
@@ -3,7 +3,35 @@ import React from "react";
 type Props = {};
 
 const LatestPosts = (props: Props) => {
-  return <div>LatestPosts</div>;
+  return (
+    <section className="py-60">
+      <div className="container mx-auto">
+        <h2 className="font-serif font-black text-4xl">
+          Your portal to the future
+        </h2>
+        <p className="text-xl text-center text-zinc-400 mt-8">
+          Keep up with the new features, updates, trends and insights in the
+          blockchain world!
+        </p>
+        <div>
+          {[...new Array(4)].fill(0).map((item, itemIndex) => (
+            <div key={itemIndex}>
+              <div>Technology</div>
+              <h3>Regular Updates</h3>
+              <p>
+                Understand the regular blockchain updates and what is means for
+                the future of blockchain
+              </p>
+              <div>
+                <button>Read more</button>
+                <div>arrow</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default LatestPosts;
